399c6173092f978e995b94d02bca63d04ea02208
xu3woli <prostyliu@gmail.com>
2024-08-31 21:30:59 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# css-unit
+# init
+
+```
+npm init -y
+npm create vite@latest
+npm install
+npm run dev
+
+//dependencies
+npm i bootstrap
+npm i vue-router
+~~npm i bootstrap-icons
+npm i --save bootstrap @popperjs/core
+
+//devDependencies
+npm i sass --save-dev
+```
---
+++ src/main.js
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+
+import '@/assets/styles/all.scss';
+import * as bootstrap from 'bootstrap'
+
+// import AOS from 'aos';
+// import 'aos/dist/aos.css'; 
+// AOS.init();
+
+const app = createApp(App)
+
+app.use(router)
+app.mount('#app')
